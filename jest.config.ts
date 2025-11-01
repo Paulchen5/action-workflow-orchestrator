2026a2fb49c5983e6eb215da2a2fa046a415e477
Paulchen <lukas.23022005@gmail.com>
2025-11-01 13:29:42 +0100

    chore: Update `testMatch` in `jest.config.ts`

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["**/test/**/*.test.ts"],
+    testMatch: ["**/tests/**/*.test.ts"],
     testTimeout: 300000,
     transform: {
         "^.+\\.ts$": [
